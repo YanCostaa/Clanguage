29f99877b244f74969ff6cde563dc951dbe43400
Yan <yan.felipe.cds@gmail.comcls>
2025-04-05 18:30:10 -0300

    pushing ex002

---
+++ Ex002.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <stdlib.h>
+//1 - Write a program that prints the messages “Início do programa” and “Fim” on the screen, one per line, using only one printf() command.
+int main(){
+    printf("Inicio do programa\nFim");
+
+    return 0;
+}
