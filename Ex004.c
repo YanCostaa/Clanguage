82ff6d6f8eab0491f8ee3f2ba92c9a6c2d9601ea
Yan <yan.felipe.cds@gmail.comcls>
2025-04-05 23:24:47 -0300

    pushing ex004

---
+++ Ex004.c
@@ -0,0 +1,10 @@
+ #include <stdio.h>
+ #include <stdlib.h>
+// 4) Write a program that reads an integer and then prints it using the '%f' operator. See what happens."
+int main(){
+    int x;
+    scanf("%d", &x);
+    printf("%f\n", x);
+
+    return 0;
+}
