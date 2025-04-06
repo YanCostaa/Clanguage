9553b072d3bd613851eb9966f019ac1c6fbc6e5a
Yan <yan.felipe.cds@gmail.comcls>
2025-04-05 23:33:47 -0300

    pushing ex005

---
+++ Ex005.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+//5) Write a program that reads a value of type float and then prints it using the %d operator. See what happens.
+int main(){
+    float x;
+    scanf("%f", &x);
+    printf("%d\n", x);
+
+    return 0;
+}
+
