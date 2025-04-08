68095dfa82eb58d67d5f35ee2c9320a7de6c1f59
Yan <yan.felipe.cds@gmail.comcls>
2025-04-07 21:47:22 -0300

    pushing ex012

---
+++ Ex012.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+//12) Write a program that contains any constant of type int. Use the const keyword. Print this constant.
+int main(){
+    const int x = 19;
+    printf("Constant: %d\n", x);
+
+    return 0;
+}
