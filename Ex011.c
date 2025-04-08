c01d4cc0808ee9174a6780ecc62d9faa755e67a6
Yan <yan.felipe.cds@gmail.comcls>
2025-04-07 21:42:04 -0300

    pushing ex011

---
+++ Ex011.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+//11) Write a program that contains any constant of type float. Use the #define directive. Print this constant.
+#define x 15.5
+int main(){
+    printf("constant: %f", x);
+
+    return 0;
+}
